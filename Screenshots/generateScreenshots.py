4a1556d9216c7b2f1ce6c436950ffcb63bd846f5
ByteHamster <info@bytehamster.com>
2020-09-27 22:38:09 +0200

    Added tablet screenshot generator

--- Screenshots/generateScreenshots.py
+++ Screenshots/generateScreenshots.py
@@ -6,11 +6,23 @@ def generateText(text, font):
     print(text)
     os.system("convert -size 1698x750 xc:none -gravity Center -pointsize 130 -fill '#167df0' -font " + font + ' -annotate 0 "' + text + '" /tmp/text.png')
 
+def generateTabletText(text, font):
+    print(text)
+    os.system("convert -size 1730x350 xc:none -gravity Center -pointsize 80 -fill '#167df0' -font " + font + ' -annotate 0 "' + text + '" /tmp/text.png')
+
 def simplePhone(text, backgroundFile, screenshotFile, outputFile, font):
     generateText(text, font)
     os.system('convert templates/' + backgroundFile
         + ' templates/phone.png -geometry +0+0 -composite '
         + screenshotFile + ' -geometry +306+992 -composite '
+        + '/tmp/text.png -geometry +0+0 -composite '
+        + outputFile)
+
+def simpleTablet(text, screenshotFile, outputFile, font):
+    generateTabletText(text, font)
+    os.system('convert ' + screenshotFile + ' -resize 1285 "/tmp/resized-image.png"')
+    os.system('convert templates/tablet.png '
+        + '/tmp/resized-image.png -geometry +224+459 -composite '
         + '/tmp/text.png -geometry +0+0 -composite '
         + outputFile)
 
@@ -40,7 +52,9 @@ def generateScreenshots(language, font):
     simplePhone(texts[2], 'background2.png', rawScreenshotsPath + '/02.png', outputPath + '/03.png', font)
     simplePhone(texts[4], 'background1.png', rawScreenshotsPath + '/04.png', outputPath + '/04.png', font)
     simplePhone(texts[5], 'background2.png', rawScreenshotsPath + '/05.png', outputPath + '/05.png', font)
-    os.system('mogrify -resize 1120 "' + outputPath + '/*.png"')
+    if len(texts) == 7:
+        simpleTablet(texts[6], rawScreenshotsPath + '/tablet.png', outputPath + '/tablet.png', font)
+    os.system('mogrify -resize 1120 "' + outputPath + '/0*.png"')
 
 generateScreenshots('de-DE', 'Sarabun-Bold')
 generateScreenshots('en-US', 'Sarabun-Bold')
