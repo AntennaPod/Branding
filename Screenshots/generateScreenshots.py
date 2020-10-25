8e4fa10f4c8730e80031b4530ceeb917319b3148
sevenmaster <sevenmaster@users.noreply.github.com>
2020-10-25 18:54:59 +0100

    requirements check for screenshot script (#5)

--- Screenshots/generateScreenshots.py
+++ Screenshots/generateScreenshots.py
@@ -1,65 +1,119 @@
 #!/bin/python
 import os
+import subprocess
+import platform
 from pathlib import Path
 
-def generateText(text, font):
+langs_and_fonts = {
+    'de-DE': 'Sarabun-Bold',
+    'en-US': 'Sarabun-Bold',
+    'fr-FR': 'Sarabun-Bold',
+    'he-IL': 'Arimo-Bold',
+    'nl-NL': 'Sarabun-Bold',
+    'it-IT': 'Sarabun-Bold',
+    'es-ES': 'Sarabun-Bold'
+}
+
+
+def generate_text(text, font):
     print(text)
-    os.system("convert -size 1698x750 xc:none -gravity Center -pointsize 130 -fill '#167df0' -font " + font + ' -annotate 0 "' + text + '" /tmp/text.png')
+    os.system(
+        "convert -size 1698x750 xc:none -gravity Center -pointsize 130 -fill '#167df0' -font "
+        + font
+        + ' -annotate 0 "'
+        + text
+        + '" /tmp/text.png')
 
-def generateTabletText(text, font):
+
+def generate_tablet_text(text, font):
     print(text)
-    os.system("convert -size 1730x350 xc:none -gravity Center -pointsize 80 -fill '#167df0' -font " + font + ' -annotate 0 "' + text + '" /tmp/text.png')
+    os.system(
+        "convert -size 1730x350 xc:none -gravity Center -pointsize 80 -fill '#167df0' -font "
+        + font
+        + ' -annotate 0 "'
+        + text
+        + '" /tmp/text.png')
 
-def simplePhone(text, backgroundFile, screenshotFile, outputFile, font):
-    generateText(text, font)
-    os.system('convert templates/' + backgroundFile
-        + ' templates/phone.png -geometry +0+0 -composite '
-        + screenshotFile + ' -geometry +306+992 -composite '
-        + '/tmp/text.png -geometry +0+0 -composite '
-        + outputFile)
 
-def simpleTablet(text, screenshotFile, outputFile, font):
-    generateTabletText(text, font)
-    os.system('convert ' + screenshotFile + ' -resize 1285 "/tmp/resized-image.png"')
+def simple_phone(text, background_file, screenshotFile, outputFile, font):
+    generate_text(text, font)
+    os.system('convert templates/' + background_file
+              + ' templates/phone.png -geometry +0+0 -composite '
+              + screenshotFile + ' -geometry +306+992 -composite '
+              + '/tmp/text.png -geometry +0+0 -composite '
+              + outputFile)
+
+
+def simple_tablet(text, screenshot_file, output_file, font):
+    generate_tablet_text(text, font)
+    os.system('convert ' + screenshot_file + ' -resize 1285 "/tmp/resized-image.png"')
     os.system('convert templates/tablet.png '
-        + '/tmp/resized-image.png -geometry +224+459 -composite '
-        + '/tmp/text.png -geometry +0+0 -composite '
-        + outputFile)
+              + '/tmp/resized-image.png -geometry +224+459 -composite '
+              + '/tmp/text.png -geometry +0+0 -composite '
+              + output_file)
 
-def twoPhones(text, rawScreenshotsPath, outputFile, font):
-    generateText(text, font)
+
+def two_phones(text, raw_screenshots_path, output_file, font):
+    generate_text(text, font)
     os.system('convert templates/background2.png '
-        + 'templates/twophones-a.png -geometry +0+10 -composite '
-        + rawScreenshotsPath + '/03a.png -geometry +119+992 -composite '
-        + 'templates/twophones-b.png -geometry +0+0 -composite '
-        + rawScreenshotsPath + '/03b.png -geometry +479+1540 -composite '
-        + '/tmp/text.png -geometry +0+0 -composite '
-        + outputFile)
+              + 'templates/twophones-a.png -geometry +0+10 -composite '
+              + raw_screenshots_path + '/03a.png -geometry +119+992 -composite '
+              + 'templates/twophones-b.png -geometry +0+0 -composite '
+              + raw_screenshots_path + '/03b.png -geometry +479+1540 -composite '
+              + '/tmp/text.png -geometry +0+0 -composite '
+              + output_file)
 
-def generateScreenshots(language, font):
+
+def generate_screenshots(language, font):
     Path('output/' + language).mkdir(parents=True, exist_ok=True)
     with open('raw/' + language + '/texts.txt') as textDefinitions:
         texts = textDefinitions.readlines()
-    rawScreenshotsPath = 'raw/' + language
-    outputPath = 'output/' + language
-    
-    if not Path(rawScreenshotsPath + '/00.png').is_file():
-        rawScreenshotsPath = 'raw/en-US'
+    raw_screenshots_path = 'raw/' + language
+    output_path = 'output/' + language
 
-    simplePhone(texts[0], 'background1.png', rawScreenshotsPath + '/00.png', outputPath + '/00.png', font)
-    twoPhones(texts[3],                      rawScreenshotsPath,             outputPath + '/01.png', font)
-    simplePhone(texts[1], 'background1.png', rawScreenshotsPath + '/01.png', outputPath + '/02.png', font)
-    simplePhone(texts[2], 'background2.png', rawScreenshotsPath + '/02.png', outputPath + '/03.png', font)
-    simplePhone(texts[4], 'background1.png', rawScreenshotsPath + '/04.png', outputPath + '/04.png', font)
-    simplePhone(texts[5], 'background2.png', rawScreenshotsPath + '/05.png', outputPath + '/05.png', font)
+    if not Path(raw_screenshots_path + '/00.png').is_file():
+        raw_screenshots_path = 'raw/en-US'
+
+    simple_phone(texts[0], 'background1.png', raw_screenshots_path + '/00.png', output_path + '/00.png', font)
+    two_phones(texts[3], raw_screenshots_path, output_path + '/01.png', font)
+    simple_phone(texts[1], 'background1.png', raw_screenshots_path + '/01.png', output_path + '/02.png', font)
+    simple_phone(texts[2], 'background2.png', raw_screenshots_path + '/02.png', output_path + '/03.png', font)
+    simple_phone(texts[4], 'background1.png', raw_screenshots_path + '/04.png', output_path + '/04.png', font)
+    simple_phone(texts[5], 'background2.png', raw_screenshots_path + '/05.png', output_path + '/05.png', font)
     if len(texts) == 7:
-        simpleTablet(texts[6], rawScreenshotsPath + '/tablet.png', outputPath + '/tablet.png', font)
-    os.system('mogrify -resize 1120 "' + outputPath + '/0*.png"')
+        simple_tablet(texts[6], raw_screenshots_path + '/tablet.png', output_path + '/tablet.png', font)
+    os.system('mogrify -resize 1120 "' + output_path + '/0*.png"')
 
-generateScreenshots('de-DE', 'Sarabun-Bold')
-generateScreenshots('en-US', 'Sarabun-Bold')
-generateScreenshots('fr-FR', 'Sarabun-Bold')
-generateScreenshots('he-IL', 'Arimo-Bold')
-generateScreenshots('nl-NL', 'Sarabun-Bold')
-generateScreenshots('it-IT', 'Sarabun-Bold')
-generateScreenshots('es-ES', 'Sarabun-Bold')
+
+def check_os():
+    """Currently only working on Linux."""
+    return platform.system() == 'Linux'
+
+
+def check_packages():
+    """ImageMagicks convert and morgify are required."""
+    common = b'Version: ImageMagick'
+    try:
+        return common in subprocess.check_output(['convert', '-version']) and common in subprocess.check_output(
+            ['mogrify', '-version'])
+    except subprocess.CalledProcessError:
+        return False
+
+
+def check_fonts():
+    """Check if required fonts are installed."""
+    try:
+        for font in langs_and_fonts.values():
+            if bytes(font.encode()) not in subprocess.check_output(['fc-list', '-v']):
+                return False
+    except subprocess.CalledProcessError:
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    assert (check_os())
+    assert (check_packages())
+    assert (check_fonts())
+    for lang, font in langs_and_fonts.items():
+        generate_screenshots(lang, font)
